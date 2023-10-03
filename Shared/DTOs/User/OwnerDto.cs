0de89e5506c0fd655e8d8061c3699cb77fcf385b
MathiasMillingHansen <331199@via.dk>
2023-10-03 17:52:51 +0200

    More blazor stuff

--- Shared/DTOs/User/OwnerDto.cs
+++ Shared/DTOs/User/OwnerDto.cs
@@ -1,11 +1,47 @@
+using System;
+using System.ComponentModel;
+
 namespace Shared.DTOs.User;
 
+[TypeConverter(typeof(OwnerDtoConverter))]
 public class OwnerDto
 {
     public string Username { get; set; }
-    
+
     public OwnerDto(string username)
     {
         Username = username;
+    }
+}
+
+public class OwnerDtoConverter : TypeConverter
+{
+    public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+    {
+        return sourceType == typeof(string);
+    }
+
+    public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+    {
+        if (value is string)
+        {
+            return new OwnerDto((string)value);
+        }
+        return base.ConvertFrom(context, culture, value);
+    }
+
+    public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+    {
+        return destinationType == typeof(string);
+    }
+
+    public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+    {
+        if (destinationType == typeof(string) && value is OwnerDto)
+        {
+            OwnerDto owner = (OwnerDto)value;
+            return owner.Username;
+        }
+        return base.ConvertTo(context, culture, value, destinationType);
     }
 }
